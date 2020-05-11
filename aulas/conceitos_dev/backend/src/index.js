a9a471c5686843010152e8b91529d86ef0ce5fd9
Jordana Momberg <jordanamomberg@gmail.com>
2020-05-10 21:17:39 -0300

    aplicacao_funcional

--- aulas/conceitos_dev/backend/src/index.js
+++ aulas/conceitos_dev/backend/src/index.js
@@ -1,9 +1,69 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+    const { title } = request.query;
+
+    console.log(projects);
+
+    const results = title 
+        ? projects.filter(project => projects.title.includes(title))
+        : projects;
+
+    return response.json(results);
 }); 
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    const { title, owner } = request.body;
+
+    const project = { id: uuid(), title, owner };
+
+    projects.push(project);
+
+    return response.json(project);
+}); 
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if ( projectIndex < 0 ) {
+        return response.status(400).json({ error: 'Project not found' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+}); 
+
+app.delete('/projects/:id', (request, response) => {
+   const { id } = request.params;
+
+   const projectIndex = projects.findIndex(project => project.id == id);
+
+   if ( projectIndex < 0 ) {
+       return response.status(400).json({ error: 'Project not found' });
+   }
+
+   projects.splice(projectIndex, 1);
+   
+    return response.status(204).send();
+}); 
+
+app.listen(3333, () => {
+    console.log('Foguetinho!')
+});
